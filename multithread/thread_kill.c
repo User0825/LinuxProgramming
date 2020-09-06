15658037a16ad0cc66c402f342621054d8451122
User0825 <2969359443@qq.com>
2020-09-06 20:06:03 +0800

    20200906

---
+++ multithread/thread_kill.c
@@ -0,0 +1,34 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <pthread.h>
+ #include <sys/types.h>  //进程头文件
+ #include <unistd.h>     //进程头文件
+ 
+ void *thread_fun(void *arg){
+	 sleep(1);
+	 printf("new thread.\n");
+	 return (void *)0;
+ }
+ 
+ int main(){
+	 pthread_t tid;
+	 int err, s;
+	 void *rval;
+	 err = pthread_create(&tid,NULL,thread_fun,NULL);
+	 if(err != 0){
+		 printf("create new thread failed.\n");
+		 return;
+	 }
+	 
+	 //sleep(1);
+	 
+	 //s = pthread_kill(tid, 0);
+	 s = pthread_kill(tid, SIGQUIT);
+	 if(s == ESRCH){
+		 printf("thread tid is not found.\n");
+	 }
+	 pthread_join(tid, &rval);
+	 printf("main thread.\n");
+	 return 0;
+ }
