08ab7e019ba8823e64822cd95b7fbb91250aaaa9
User0825 <2969359443@qq.com>
2020-09-07 21:24:03 +0800

    20200907

--- multithread/mutex.c
+++ multithread/mutex.c
@@ -52,18 +52,18 @@
 	 
 	 err = pthread_create(&tid1, NULL, thread_fun1, NULL);
 	 if(err != 0){
-		 printf("create new thread 1 failed!\n);
+		 printf("create new thread 1 failed!\n");
 		 return 0;
 	 }
 	 
 	 err = pthread_create(&tid2, NULL, thread_fun2, NULL);
 	 if(err != 0){
-		 printf("create new thread 2 failed!\n);
+		 printf("create new thread 2 failed!\n");
 		 return 0;
 	 }
 	 
 	 pthread_join(tid1, NULL);
-	 pthread_join(tid2, NULL;
+	 pthread_join(tid2, NULL);
 	 
 	 return 0;
  }
